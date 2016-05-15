c096aa46c430277720e9255d20048993e837dabd
knexer <knexer@gmail.com>
2016-05-15 15:40:46 -0700

    Simple picking-stuff-up test

--- Assets/Scripts/PickUpParent.cs
+++ Assets/Scripts/PickUpParent.cs
@@ -1,20 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(SteamVR_TrackedObject))]
 public class PickUpParent : MonoBehaviour {
 
     private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device device;
+
+    private HashSet<GameObject> grabbedChildren;
 
 	void Awake () {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
+        grabbedChildren = new HashSet<GameObject>();
 	}
 	
 	void FixedUpdate () {
-        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
-        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
         {
-            Debug.Log("Trigger is depressed.");
+            foreach (GameObject formerlyGrabbed in grabbedChildren)
+            {
+                formerlyGrabbed.transform.SetParent(null, true);
+                formerlyGrabbed.GetComponent<Rigidbody>().isKinematic = false;
+            }
+
+            grabbedChildren.Clear();
         }
 	}
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Trigger colliding with " + other.name);
+
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger) && ! grabbedChildren.Contains(other.gameObject))
+        {
+            other.transform.parent = transform;
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            grabbedChildren.Add(other.gameObject);
+        }
+    }
 }
