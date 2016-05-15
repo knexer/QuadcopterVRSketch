5559156be7a383f6813d3202c566e53f59449061
knexer <knexer@gmail.com>
2016-05-15 15:18:01 -0700

    Test script to detect trigger press

---
+++ Assets/Scripts/PickUpParent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickUpParent : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+
+	void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void FixedUpdate () {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Trigger is depressed.");
+        }
+	}
+}
