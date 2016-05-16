76902b1ecc808f4037f3ef1db9183d1ce6bc34ad
knexer <knexer@gmail.com>
2016-05-15 22:30:56 -0700

    Throttle control with trigger

--- Assets/PBR Racing Drone/Scripts/DroneAnimator.cs
+++ Assets/PBR Racing Drone/Scripts/DroneAnimator.cs
@@ -28,14 +28,5 @@ public class DroneAnimator : MonoBehaviour {
 		foreach (Transform prop in Props) {
 			prop.Rotate (0, 0, propSpeed * Time.deltaTime);
 		}
-		Vector3 pos = trans.localPosition;
-		pos.y = Mathf.Sin (Time.time * bobSpeed) * bobHeight;
-		trans.localPosition = pos;
-
-		Vector3 rot = trans.localEulerAngles;
-		rot.x = 270 +  Mathf.Sin (Time.time * wobbleSpeed) * wobble;
-		rot.z = Mathf.Cos (Time.time * wobbleSpeed) * wobble;
-		rot.y = 0;
-		trans.localEulerAngles = rot;
 	}
 }
--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 [RequireComponent(typeof(SteamVR_TrackedObject))]
 public class DroneController : MonoBehaviour {
-    ///// CONFIGURABLES /////
+    ///// HOOKUP CONFIGURABLES /////
     public GameObject Drone;
     public DroneAnimator Animator;
+
+    ///// BEHAVIOR CONFIGURABLES /////
+    public float MaxForce;
 
     ///// INFERRED CONFIGURATION /////
     private SteamVR_Controller.Device _device;
@@ -25,7 +28,19 @@ public class DroneController : MonoBehaviour {
     {
         _device = SteamVR_Controller.Input((int)_controller.index);
 
-        Vector2 throttle = _device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis1);
-        Debug.Log("Trigger state: (" + throttle.x + ", " + throttle.y + ")");
+        handleThrottle();
 	}
+
+    void handleThrottle()
+    {
+        // get trigger degree of activation
+        float throttle = _device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis1).x;
+
+        // compute force direction + magnitude
+        Vector3 localSpaceForce = new Vector3(0, 0, throttle * MaxForce);
+        Vector3 worldSpaceForce = Drone.transform.TransformDirection(localSpaceForce);
+
+        // apply to drone
+        Drone.GetComponent<Rigidbody>().AddForce(worldSpaceForce);
+    }
 }
