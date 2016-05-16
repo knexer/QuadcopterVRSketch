08e74d4e7a9129ddd11e93a37b87fee219bfa09a
knexer <knexer@gmail.com>
2016-05-15 21:55:33 -0700

    Add new drone model from asset store

---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class DroneController : MonoBehaviour {
+    ///// CONFIGURABLES /////
+    public GameObject Drone;
+    public DroneAnimator Animator;
+
+    ///// INFERRED CONFIGURATION /////
+    private SteamVR_Controller.Device _device;
+    private SteamVR_TrackedObject _controller;
+
+    ///// STATE /////
+
+
+	// Use this for initialization
+    void Awake()
+    {
+        _controller= GetComponent<SteamVR_TrackedObject>();
+        _device = SteamVR_Controller.Input((int)_controller.index);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector2 throttle = _device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis1);
+        Debug.Log("Trigger state: (" + throttle.x + ", " + throttle.y + ")");
+	}
+}
